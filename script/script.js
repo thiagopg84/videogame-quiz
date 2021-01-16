644a3d65c28a945aa77da0ab1faf4d6b4e5741d9
thiagopg84 <57367161+thiagopg84@users.noreply.github.com>
2021-01-16 20:51:34 -0300

    first push

---
+++ script/script.js
@@ -0,0 +1,497 @@
+const myObj = [
+    {
+        id: '001',
+        answers: ['ATARI'],
+        imgA: 'img/q01a.jpg',
+        imgB: 'img/q01b.jpg',
+        img: 'img/q01.png'
+    },
+
+    {
+        id: '002',
+        answers: ['NINTENDO', 'NES'],
+        imgA: 'img/q02a.jpg',
+        imgB: 'img/q02b.jpg',
+        img: 'img/q02.png'
+    },
+
+    {
+        id: '003',
+        answers: ['MASTER SYSTEM'],
+        imgA: 'img/q03a.jpg',
+        imgB: 'img/q03b.jpg',
+        img: 'img/q03.png'
+    },
+
+    {
+        id: '004',
+        answers: ['GAME BOY'],
+        imgA: 'img/q04a.jpg',
+        imgB: 'img/q04b.jpg',
+        img: 'img/q04.png'
+    },
+    
+    {
+        id: '005',
+        answers: ['GAME GEAR'],
+        imgA: 'img/q05a.jpg',
+        imgB: 'img/q05b.jpg',
+        img: 'img/q05.png'
+    },
+    
+    {
+        id: '006',
+        answers: ['MEGA DRIVE', 'GENESIS', 'SEGA GENESIS', 'SEGA MEGADRIVE'],
+        imgA: 'img/q06a.jpg',
+        imgB: 'img/q06b.jpg',
+        img: 'img/q06.png'
+    },
+    
+    {
+        id: '007',
+        answers: ['NEO GEO'],
+        imgA: 'img/q07a.jpg',
+        imgB: 'img/q07b.jpg',
+        img: 'img/q07.png'
+    },
+    
+    {
+        id: '008',
+        answers: ['SUPER NINTENDO', 'SNES'],
+        imgA: 'img/q08a.jpg',
+        imgB: 'img/q08b.jpg',
+        img: 'img/q08.png'
+    },
+    
+    {
+        id: '009',
+        answers: ['SEGA CD'],
+        imgA: 'img/q09a.jpg',
+        imgB: 'img/q09b.jpg',
+        img: 'img/q09.png'
+    },
+    
+    {
+        id: '010',
+        answers: ['SEGA 32X'],
+        imgA: 'img/q10a.jpg',
+        imgB: 'img/q10b.jpg',
+        img: 'img/q10.png'
+    },
+    
+    {
+        id: '011',
+        answers: ['3DO'],
+        imgA: 'img/q11a.jpg',
+        imgB: 'img/q11b.jpg',
+        img: 'img/q11.png'
+    },
+    
+    {
+        id: '012',
+        answers: ['AMIGA CD32'],
+        imgA: 'img/q12a.jpg',
+        imgB: 'img/q12b.jpg',
+        img: 'img/q12.png'
+    },
+    
+    {
+        id: '013',
+        answers: ['ATARI JAGUAR', 'JAGUAR'],
+        imgA: 'img/q13a.jpg',
+        imgB: 'img/q13b.jpg',
+        img: 'img/q13.png'
+    },
+    
+    {
+        id: '014',
+        answers: ['SEGA SATURN', 'SATURN'],
+        imgA: 'img/q14a.jpg',
+        imgB: 'img/q14b.jpg',
+        img: 'img/q14.png'
+    },
+    
+    {
+        id: '015',
+        answers: ['PLAYSTATION', 'SONY PLAYSTATION'],
+        imgA: 'img/q15a.jpg',
+        imgB: 'img/q15b.jpg',
+        img: 'img/q15.png'
+    },
+    
+    {
+        id: '016',
+        answers: ['VIRTUAL BOY'],
+        imgA: 'img/q16a.jpg',
+        imgB: 'img/q16b.jpg',
+        img: 'img/q16.png'
+    },
+    
+    {
+        id: '017',
+        answers: ['NINTENDO 64', 'N64'],
+        imgA: 'img/q17a.jpg',
+        imgB: 'img/q17b.jpg',
+        img: 'img/q17.png'
+    },
+    
+    {
+        id: '018',
+        answers: ['DREAMCAST', 'SEGA DREAMCAST'],
+        imgA: 'img/q18a.jpg',
+        imgB: 'img/q18b.jpg',
+        img: 'img/q18.png'
+    },
+    
+    {
+        id: '019',
+        answers: ['NINTENDO GAMECUBE', 'GAMECUBE'],
+        imgA: 'img/q19a.jpg',
+        imgB: 'img/q19b.jpg',
+        img: 'img/q19.png'
+    },
+    
+    {
+        id: '020',
+        answers: ['GAMEBOY ADVANCE', 'GBA'],
+        imgA: 'img/q20a.jpg',
+        imgB: 'img/q20b.jpg',
+        img: 'img/q20.png'
+    },
+    
+    {
+        id: '021',
+        answers: ['PSP', 'SONY PSP'],
+        imgA: 'img/q21a.jpg',
+        imgB: 'img/q21b.jpg',
+        img: 'img/q21.png'
+    },
+    
+    {
+        id: '022',
+        answers: ['NINTENDO WII', 'WII'],
+        imgA: 'img/q22a.jpg',
+        imgB: 'img/q22b.jpg',
+        img: 'img/q22.png'
+    },
+    
+    {
+        id: '023',
+        answers: ['XBOX', 'MICROSOFT XBOX'],
+        imgA: 'img/q23a.jpg',
+        imgB: 'img/q23b.jpg',
+        img: 'img/q23.png'
+    }
+];
+
+// OLD VERSION (2 images)
+
+// const myObj = [
+//     {
+//         id: '001',
+//         answers: 'ATARI',
+//         imgA: 'img/q01a.jpg',
+//         imgB: 'img/q01b.jpg',
+//         img: 'img/q01.png'
+//     },
+
+//     {
+//         id: '002',
+//         answers: 'NINTENDO',
+//         imgA: 'img/q02a.jpg',
+//         imgB: 'img/q02b.jpg',
+//         img: 'img/q02.png'
+//     },
+
+//     {
+//         id: '003',
+//         answers: 'MASTER SYSTEM',
+//         imgA: 'img/q03a.jpg',
+//         imgB: 'img/q03b.jpg',
+//         img: 'img/q03.png'
+//     },
+
+//     {
+//         id: '004',
+//         answers: 'GAME BOY',
+//         imgA: 'img/q04a.jpg',
+//         imgB: 'img/q04b.jpg',
+//         img: 'img/q04.png'
+//     },
+    
+//     {
+//         id: '005',
+//         answers: 'GAME GEAR',
+//         imgA: 'img/q05a.jpg',
+//         imgB: 'img/q05b.jpg',
+//         img: 'img/q05.png'
+//     },
+    
+//     {
+//         id: '006',
+//         answers: 'MEGA DRIVE',
+//         imgA: 'img/q06a.jpg',
+//         imgB: 'img/q06b.jpg',
+//         img: 'img/q06.png'
+//     },
+    
+//     {
+//         id: '007',
+//         answers: 'NEO GEO',
+//         imgA: 'img/q07a.jpg',
+//         imgB: 'img/q07b.jpg',
+//         img: 'img/q07.png'
+//     },
+    
+//     {
+//         id: '008',
+//         answers: 'SUPER NINTENDO',
+//         imgA: 'img/q08a.jpg',
+//         imgB: 'img/q08b.jpg',
+//         img: 'img/q08.png'
+//     },
+    
+//     {
+//         id: '009',
+//         answers: 'SEGA CD',
+//         imgA: 'img/q09a.jpg',
+//         imgB: 'img/q09b.jpg',
+//         img: 'img/q09.png'
+//     },
+    
+//     {
+//         id: '010',
+//         answers: 'SEGA 32X',
+//         imgA: 'img/q10a.jpg',
+//         imgB: 'img/q10b.jpg',
+//         img: 'img/q10.png'
+//     },
+    
+//     {
+//         id: '011',
+//         answers: '3DO',
+//         imgA: 'img/q11a.jpg',
+//         imgB: 'img/q11b.jpg',
+//         img: 'img/q11.png'
+//     },
+    
+//     {
+//         id: '012',
+//         answers: 'AMIGA CD32',
+//         imgA: 'img/q12a.jpg',
+//         imgB: 'img/q12b.jpg',
+//         img: 'img/q12.png'
+//     },
+    
+//     {
+//         id: '013',
+//         answers: 'ATARI JAGUAR',
+//         imgA: 'img/q13a.jpg',
+//         imgB: 'img/q13b.jpg',
+//         img: 'img/q13.png'
+//     },
+    
+//     {
+//         id: '014',
+//         answers: 'SEGA SATURN',
+//         imgA: 'img/q14a.jpg',
+//         imgB: 'img/q14b.jpg',
+//         img: 'img/q14.png'
+//     },
+    
+//     {
+//         id: '015',
+//         answers: 'PLAYSTATION',
+//         imgA: 'img/q15a.jpg',
+//         imgB: 'img/q15b.jpg',
+//         img: 'img/q15.png'
+//     },
+    
+//     {
+//         id: '016',
+//         answers: 'VIRTUAL BOY',
+//         imgA: 'img/q16a.jpg',
+//         imgB: 'img/q16b.jpg',
+//         img: 'img/q16.png'
+//     },
+    
+//     {
+//         id: '017',
+//         answers: 'NINTENDO 64',
+//         imgA: 'img/q17a.jpg',
+//         imgB: 'img/q17b.jpg',
+//         img: 'img/q17.png'
+//     },
+    
+//     {
+//         id: '018',
+//         answers: 'DREAMCAST',
+//         imgA: 'img/q18a.jpg',
+//         imgB: 'img/q18b.jpg',
+//         img: 'img/q18.png'
+//     },
+    
+//     {
+//         id: '019',
+//         answers: 'NINTENDO GAMECUBE',
+//         imgA: 'img/q19a.jpg',
+//         imgB: 'img/q19b.jpg',
+//         img: 'img/q19.png'
+//     },
+    
+//     {
+//         id: '020',
+//         answers: 'GAMEBOY ADVANCE',
+//         imgA: 'img/q20a.jpg',
+//         imgB: 'img/q20b.jpg',
+//         img: 'img/q20.png'
+//     },
+    
+//     {
+//         id: '021',
+//         answers: 'PSP',
+//         imgA: 'img/q21a.jpg',
+//         imgB: 'img/q21b.jpg',
+//         img: 'img/q21.png'
+//     },
+    
+//     {
+//         id: '022',
+//         answers: 'NINTENDO WII',
+//         imgA: 'img/q22a.jpg',
+//         imgB: 'img/q22b.jpg',
+//         img: 'img/q22.png'
+//     },
+    
+//     {
+//         id: '023',
+//         answers: 'XBOX',
+//         imgA: 'img/q23a.jpg',
+//         imgB: 'img/q23b.jpg',
+//         img: 'img/q23.png'
+//     }
+// ];
+
+const inputArea = document.querySelector('#myInput');
+const score = document.querySelector('#score');
+const outputDiv = document.querySelector('.cards');
+const pOutput = document.querySelector('#pOutput');
+let htmlOutput = '';
+
+score.innerText = +myObj.length;
+
+// for (let i = 0; i < myObj.length; i++) {
+//     htmlOutput += `
+//             <div class="card">
+//                 <div class="card__img">
+//                     <img src="${myObj[i].imgA}" alt="${myObj[i].id}" class="console-image">
+//                 </div>
+//                 <p class="resp"></p>
+//             </div>
+//             `;
+// };
+
+myObj.forEach((e) => {
+    htmlOutput += `
+    <div class="card">
+        <div class="card__img">
+            <img src="${e.img}" alt="${e.id}" class="console-image dark-image">
+        </div>
+        <p class="resp"></p>
+    </div>
+    `
+
+    // OLD VERSION (2 images)
+    
+    // htmlOutput += `
+    // <div class="card">
+    //     <div class="card__img">
+    //         <img src="${e.imgA}" alt="${e.id}" class="console-image">
+    //     </div>
+    //     <p class="resp"></p>
+    // </div>
+    // `
+})
+
+outputDiv.innerHTML = htmlOutput;
+const paragraphsAnswers = document.querySelectorAll('.resp');
+const allImages = document.querySelectorAll('.console-image');
+
+allImages.forEach(e => {
+    e.ondragstart = () => {
+        return false;
+    }
+})
+
+inputArea.addEventListener('keyup', () => {
+    for (let i = 0; i < myObj.length; i++) {
+        if (paragraphsAnswers[i].innerText == '') {
+            // console.log(`parágrafo ${i + 1} tá vazio`)
+            
+            // if (myObj[i].answers.includes(inputArea.value.toUpperCase().trim().replace(/\s+/g, '')) || myObj[i].answers.includes(inputArea.value.toUpperCase().trim())) {
+                //     paragraphsAnswers[i].innerText = myObj[i].answers[0];
+                //     allImages[i].classList.remove('dark-image');
+                //     inputArea.value = '';
+                //     score.innerText--;
+                // }
+
+            // for (let answer of myObj[i].answers) {
+            //     if (inputArea.value.toUpperCase().trim().replace(/\s+/g, '') == answer.trim().replace(/\s+/g, '')) {
+            //         paragraphsAnswers[i].innerText = myObj[i].answers[0];
+            //         allImages[i].classList.remove('dark-image');
+            //         inputArea.value = '';
+            //         score.innerText--;
+            //     }
+            // }
+            
+            myObj[i].answers.forEach(e => {
+                if (e.trim().replace(/\s+/g, '') == inputArea.value.toUpperCase().trim().replace(/\s+/g, '')) {
+                    paragraphsAnswers[i].innerText = myObj[i].answers[0];
+                    allImages[i].classList.remove('dark-image');
+                    inputArea.value = '';
+                    score.innerText--;
+                }
+            });
+
+            // Com .includes()
+            // myObj.forEach((e, index) => {
+            //     if (paragraphsAnswers[index].innerText == '') {
+            //         if (e.answers.includes(inputArea.value.toUpperCase().trim().replace(/\s+/g, ''))) {
+            //             paragraphsAnswers[index].innerText = myObj[index].answers[0];
+            //             allImages[index].classList.remove('dark-image');
+            //             inputArea.value = '';
+            //             score.innerText--;
+            //         }
+            //     }
+            // })
+
+            if (score.innerText == 0) {
+                document.activeElement.blur();
+                inputArea.setAttribute('disabled', 'true');
+                pOutput.innerHTML = 'Congratulations!';
+            };
+            
+        }    
+    };
+});
+
+// OLD VERSION (2 images)
+
+// inputArea.addEventListener('keyup', () => {
+//     for (let i = 0; i < myObj.length; i++) {
+//         if (paragraphsAnswers[i].innerText == '') {
+//             console.log(`parágrafo ${i + 1} tá vazio`)
+//             if (inputArea.value.toUpperCase().trim().replace(/\s+/g, '') == myObj[i].answer.replace(/\s+/g, '')) {
+//                 paragraphsAnswers[i].innerText = myObj[i].answer;
+//                 allImages[i].setAttribute('src', myObj[i].img);
+//                 inputArea.value = '';
+//                 score.innerText--;
+//                 if (score.innerText == 0) {
+//                     document.activeElement.blur();
+//                     inputArea.setAttribute('disabled', 'true');
+//                     pOutput.innerHTML = 'Congratulations!';
+//                 };
+//             }
+//         }    
+//     };
+// });
